c9eb698198b61987ce09fe638075e870ab944968
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2022-08-18 14:06:26 +0200

    finished, last commit

--- AskMeAnything/ViewController.swift
+++ AskMeAnything/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ball_image_view: UIImageView!
+    let balls = ["ball1","ball2", "ball3", "ball4", "ball5", "ball6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        newBallImage()
     }
-
-
+    
+    @IBAction func grillMeOnTouch(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        let randomBallIndex = arc4random_uniform(UInt32(balls.count)) //fill the randBallIndex variable with rands
+        
+        print("ball index: \(randomBallIndex)")
+        
+        let randImage = balls[Int(randomBallIndex)] //ugyan olyan tipus lesz mint amit ad
+        
+        ball_image_view.image = UIImage(named: randImage) //itt megkapjuk a nevet, hogy melyiket kell megjeleniteni.
+    }
 }
 
